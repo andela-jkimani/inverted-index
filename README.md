e6ca2e56def8c3d78a1ab26ccb129fe0912ae90f
Jacky <jackline.kimani@andela.com>
2016-06-23 23:58:35 +0300

    Modify unique words for-loop and README

--- README.md
+++ README.md
@@ -2,10 +2,28 @@
 
 The inverted index is a structure used to perform the elasticsearch on a document to allow for fast searches. It contains the list of all unique words that appear on a document.
 
+------------------------------------------------------
+
 ## Steps to using the inverted index
 1. Clone the repo by running the command `git clone git@github.com:andela-jkimani/inverted-index.git`
 2. Install http-server by running the command `npm install -g http-server`
 3. Run tests by using the following steps:
 	* cd into the folder `inverted-index`
 	* Run the command `http-server` to start the server.
-	* Open chrome and go to the URL `http://localhost:8000/jasmine/SpecRunner.html`
+	* Open chrome and go to the URL `http://localhost:8080/jasmine/SpecRunner.html`
+
+-------------------------------------------------------
+
+## Index functions
+
+### loadData(filePath)
+Takes in the file path as its parameters and returns a JSON array
+
+### createIndex(books)
+Takes in the JSON array as the argument and creates its inverted index
+
+### getIndex()
+Returns the inverted index when called and takes in no parameters
+
+### searchIndex(terms)
+Takes in one or more words (string or array) and returns their indexes after searching the index
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,89 +1,84 @@
 function Index() {
-    this.index = {};
+  this.index = {};
 
-    //load the JSON file
-    this.loadData = function(filePath) {
-        var request = new XMLHttpRequest();
-        request.open('GET', filePath, false);
-        request.send(null);
-        if (request.status === 200) {
-            if (request.responseText.trim().length === 0) {
-                throw new Error('the file is empty');
-            }
+  //load the JSON file
+  this.loadData = function(filePath) {
+    var request = new XMLHttpRequest();
+    request.open('GET', filePath, false);
+    request.send(null);
+    if (request.status === 200) {
+      if (request.responseText.trim().length === 0) {
+        throw new Error('the file is empty');
+      }
 
-            try {
-                return JSON.parse(request.responseText);
-            } catch (e) {
-                throw new Error('invalid json file');
-            }
+      try {
+        return JSON.parse(request.responseText);
+      } catch (e) {
+        throw new Error('invalid json file');
+      }
+    }
+
+    throw new Error('unable to open file');
+  };
+
+  //reading the json file
+  this.createIndex = function(books) {
+    var uniqueWords = [];
+    self = this;
+
+    //removing punctuation marks, splitting and concatenating the strings 
+    for (i = 0; i < books.length; i++) {
+      var content = (books[i].title + ' ' + books[i].text)
+        .replace(/\W+/g, ' ')
+        .toLowerCase()
+        .trim()
+        .split(' ');
+
+      //getting unique words
+      var words = content.filter(function(word, key) {
+        return content.indexOf(word) == key;
+      });
+      uniqueWords.push(words);
+    }
+
+    //creating index
+    for (i = 0; i < uniqueWords.length; i++) {
+      for (var j = 0; j < uniqueWords[i].length; j++) {
+        if (!(uniqueWords[i][j] in self.index)) {
+          self.index[uniqueWords[i][j]] = [i];
+        } else {
+          self.index[uniqueWords[i][j]].push(i);
         }
 
-        throw new Error('unable to open file');
-    };
+      }
+    }
+  };
 
-    //reading the json file
-    this.createIndex = function(books) {
-        var newArray = [];
-        var uniqueWords = [];
-        self = this;
+  //getting the index
+  this.getIndex = function() {
+    return self.index;
+  };
 
-        //removing punctuation marks, splitting and concatenating the strings 
-        for (i = 0; i < books.length; i++) {
-            content = (books[i].title + ' ' + books[i].text)
-                .replace(/\W+/g, ' ')
-                .toLowerCase()
-                .trim()
-                .split(' ');
-            newArray.push(content);
-        }
+  //searching for specific terms
+  this.searchIndex = function(terms) {
+    var self = this,
+      results = [];
 
-        //getting unique words
-        for (i = 0; i < newArray.length; i++) {
-            var words = newArray[i].filter(function(word, key) {
-                return newArray[i].indexOf(word) == key;
-            });
+    if (!Array.isArray(terms)) {
+      terms = terms
+        .toLowerCase()
+        .replace(/[,]/g, '')
+        .split(' ');
+    }
 
-            uniqueWords.push(words);
-        }
+    terms.forEach(function(word) {
+      if (self.index.hasOwnProperty(word)) {
+        results.push(self.index[word]);
+      } else {
+        results.push('Not found');
+      }
+    });
 
-        //creating index
-        for (i = 0; i < uniqueWords.length; i++) {
-            for (var j = 0; j < uniqueWords[i].length; j++) {
-                if (!(uniqueWords[i][j] in self.index)) {
-                    self.index[uniqueWords[i][j]] = [i];
-                } else {
-                    self.index[uniqueWords[i][j]].push(i);
-                }
-
-            }
-        }
-    };
-
-    //getting the index
-    this.getIndex = function() {
-        return self.index;
-    };
-
-    //searching for specific terms
-    this.searchIndex = function(terms) {
-        var self = this,
-            results = [];
-
-        if (!Array.isArray(terms)) {
-            terms = terms
-                .toLowerCase()
-                .replace(/[,]/g, '')
-                .split(' ');
-        }
-
-        terms.forEach(function(word) {
-            if (self.index.hasOwnProperty(word)) {
-                results.push(self.index[word]);
-            } else {
-                results.push('Not found');
-            }
-        });
-
-        return results;
-    };
+    return results;
+  };
 }
