b260f562ee1f59189b5f03b84a96af544565626b
Jacky <jackline.kimani@andela.com>
2016-06-30 11:33:49 +0300

    Make tests asynchronous

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,33 +1,20 @@
-describe('Inverted Index', function() {
+describe("Inverted Index", function() {
   var books,
-    myIndex,
-    index;
-
-  beforeEach(function() {
+    index,
     myIndex = new Index();
-    books = myIndex.loadData('../jasmine/books.json');
-    myIndex.createIndex(books);
-    index = myIndex.getIndex();
+
+  beforeAll(function(done) {
+    myIndex.loadData('books.json', function(response) {
+      books = response;
+      myIndex.createIndex(books);
+      index = myIndex.getIndex();
+      done();
+    });
   });
 
   describe('File loading test', function() {
     it('should return an array if loaded properly', function() {
       expect(Array.isArray(books)).toBeTruthy();
-    });
-    it('should check whether file is empty', function() {
-      expect(function() {
-        myIndex.loadData('../jasmine/empty_file.json');
-      }).toThrow(new Error('the file is empty'));
-    });
-    it('should throw an error when file cannot be opened', function() {
-      expect(function() {
-        myIndex.loadData('non_existent.json');
-      }).toThrow(new Error('unable to open file'));
-    });
-    it('should throw an error for an invalid json file', function() {
-      expect(function() {
-        myIndex.loadData('../jasmine/invalid.json');
-      }).toThrow(new Error('invalid json file'));
     });
   });
 
@@ -35,6 +22,7 @@ describe('Inverted Index', function() {
     it('should check whether array is empty', function() {
       expect(books.length).not.toEqual(0);
     });
+
     it('should ensure each object contains a string property', function() {
       expect(typeof books[0].title).toBe('string');
       expect(typeof books[0].text).toBe('string');
@@ -44,9 +32,11 @@ describe('Inverted Index', function() {
   });
 
   describe('Populate Index', function() {
+
     it('should ensure index is created', function() {
       expect(index).toBeDefined();
     });
+
     it('should ensure index is correct', function() {
       expect(index.falls).toEqual([0]);
       expect(index.rabbit).toEqual([0]);
@@ -63,34 +53,43 @@ describe('Inverted Index', function() {
       expect(myIndex.searchIndex('wizard')).toEqual([
         [1]
       ]);
+
       expect(myIndex.searchIndex('world')).toEqual([
         [0]
       ]);
+
       expect(myIndex.searchIndex('hello')).toEqual(['Not found']);
     });
+
     it("should ensure index returns the correct results when searched", function() {
       expect(myIndex.searchIndex(['alice', 'a'])).toEqual([
         [0, 1],
         [0, 1]
       ]);
+
       expect(myIndex.searchIndex('a')).toEqual([
         [0, 1]
       ]);
+
       expect(myIndex.searchIndex('jacky')).toEqual(['Not found']);
       expect(myIndex.searchIndex('man')).toEqual([
         [1]
       ]);
+
       expect(myIndex.searchIndex('fellowship')).toEqual([
         [1]
       ]);
+
       expect(myIndex.searchIndex('Hole')).toEqual([
         [0]
       ]);
     });
+
     it("should ensure searchIndex can handle an array of search terms", function() {
       expect(myIndex.searchIndex(['alice'])).toEqual([
         [0, 1]
       ]);
+
       expect(myIndex.searchIndex(['alice', 'jacky'])).toEqual([
         [0, 1], 'Not found'
       ]);
